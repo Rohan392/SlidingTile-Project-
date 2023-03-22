7e7f7068b4354aeaf0b3fb14905634e500233096
MarcusCaparini <Marcus.Caparini@gmail.com>
2023-03-22 11:46:11 -0600

    Sliding Solver stuff

--- include/Sliding_Solver.h
+++ include/Sliding_Solver.h
@@ -26,20 +26,20 @@ using namespace std;
  void percolateDown(int hole);
  };
  */
-template<typename Comparable>
+//template<typename Comparable>
 class BinaryHeap {
 public:
-	explicit BinaryHeap(int capacity = 100) :
-			array(capacity + 1), currentSize { 0 } {
+	BinaryHeap() :
+			currentSize { 0 } {
 	}
-
-	explicit BinaryHeap(const vector<Comparable> &items) :
-			array(items.size() + 10), currentSize { items.size() } {
+/*
+	BinaryHeap(const vector<BoardTile> &items) :
+			currentSize { items.size() } {
 		for (int i = 0; i < items.size(); ++i)
 			array[i + 1] = items[i];
 		buildHeap();
 	}
-
+*/
 	bool isEmpty() const {
 		return currentSize == 0;
 	}
@@ -48,7 +48,7 @@ public:
 	 * Find the smallest item in the priority queue.
 	 * Return the smallest item, or throw Underflow if empty.
 	 */
-	const Comparable& findMin() const {
+	const BoardTile& findMin() const {
 		if (isEmpty())
 			cout << "The Heap is empty, we cant find min";
 		return array[1];
@@ -57,7 +57,7 @@ public:
 	/**
 	 * Insert item x, allowing duplicates.
 	 */
-	void insert(const Comparable &x) {
+/*	void insert(const Comparable &x) {
 		if (currentSize == array.size() - 1)
 			array.resize(array.size() * 2);
 
@@ -69,12 +69,12 @@ public:
 		for (; x < array[hole / 2]; hole /= 2)
 			array[hole] = std::move(array[hole / 2]);
 		array[hole] = std::move(array[0]);
-	}
+	}*/
 
 	/**
 	 * Insert item x, allowing duplicates.
 	 */
-	void insert(Comparable &&x) {
+/*	void insert(Comparable &&x) {
 		if (currentSize == array.size() - 1)
 			array.resize(array.size() * 2);
 
@@ -84,24 +84,24 @@ public:
 			array[hole] = std::move(array[hole / 2]);
 		array[hole] = std::move(x);
 	}
-
+*/
 	/**
 	 * Remove the minimum item.
 	 * Throws UnderflowException if empty.
 	 */
-	void deleteMin() {
+/*	void deleteMin() {
 		if (isEmpty())
 			cout << "The Heap is empty, we cant delete min";
 
 		array[1] = std::move(array[currentSize--]);
 		percolateDown(1);
 	}
-
+*/
 	/**
 	 * Remove the minimum item and place it in minItem.
 	 * Throws Underflow if empty.
 	 */
-	void deleteMin(Comparable &minItem) {
+/*	void deleteMin(Comparable &minItem) {
 		if (isEmpty())
 			cout << "The Heap is empty, we cant delete min";
 
@@ -113,27 +113,27 @@ public:
 	void makeEmpty() {
 		currentSize = 0;
 	}
-
+*/
 private:
 	int currentSize;  // Number of elements in heap
-	vector<Comparable> array;        // The heap array
+	vector<BoardTile> array;        // The heap array
 
 	/**
 	 * Establish heap order property from an arbitrary
 	 * arrangement of items. Runs in linear time.
 	 */
-	void buildHeap() {
+/*	void buildHeap() {
 		for (int i = currentSize / 2; i > 0; --i)
 			percolateDown(i);
 	}
-
+*/
 	/**
 	 * Internal method to percolate down in the heap.
 	 * hole is the index at which the percolate begins.
 	 */
 	void percolateDown(int hole) {
 		int child;
-		Comparable tmp = std::move(array[hole]);
+		BoardTile tmp = std::move(array[hole]);
 
 		for (; hole * 2 <= currentSize; hole = child) {
 			child = hole * 2;
@@ -157,7 +157,7 @@ public:
 private:
   //bool shmall(BoardTile &a, BoardTile &b);
   vector<BoardTile> previousPlaces;
-	BinaryHeap<BoardTile*> tileQueue;
+	BinaryHeap tileQueue;
   void makeSorted();
   int hasBeen(vector<BoardTile> &Tiles, BoardTile a);
 };
